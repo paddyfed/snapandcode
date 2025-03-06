afa212c1123e6abd10c43eed8b958a2a720e147a
Paddy <paddyfedoo@hotmail.com>
2025-03-06 00:56:17 +0000

    Added logo and Metadata

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,31 @@ import "./globals.css";
 export const metadata = {
   title: "Snap and Code",
   description: "Snap and Code - Website and Photography Services",
+  keywords: [
+    "Photographer",
+    "Photography",
+    "Website",
+    "Website design",
+    "Website development",
+    "Videography",
+    "Video Editing",
+  ],
+  generator: "Next.js",
+  authors: [{ name: "Paddy O'Hara" }],
+  creator: "Paddy O'Hara",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
 import styles from "@/app/page.module.css";
+import Image from "next/image";
+import MainLogo from "@/public/snap-and-code_main-logo.svg";
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <h1>Snap and Code</h1>
+        <h1>
+          <Image
+            priority
+            src={MainLogo}
+            alt="Snap and Code"
+            width={160}
+            height={160}
+            className={styles.mainlogo}
+          />
+        </h1>
       </main>
       <footer></footer>
     </>
