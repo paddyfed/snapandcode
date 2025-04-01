b329551fb3fcf3c4062fa8567fe74edf9d0ae2ef
Paddy <paddyfedoo@hotmail.com>
2025-04-01 23:32:29 +0100

    Keeping footer at the bottom of the screen

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="d-flex flex-column">
         <NavBar />
         {children}
         <BootstrapClient />
