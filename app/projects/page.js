646baa90bb84da1996d2d234afa4dbeff8513deb
Paddy <paddyfedoo@hotmail.com>
2026-04-02 21:57:47 +0100

    March 2026

--- app/projects/page.js
+++ app/projects/page.js
@@ -15,6 +15,7 @@ import NovemberBoard2025 from "@/public/2025-11-01_Work-Board@0.25x.png";
 import DecemberBoard2025 from "@/public/2025-12-01_Work-Board@0.25x.png";
 import JanuaryBoard2026 from "@/public/2026-01-01_Work-Board@0.25x.png";
 import FebruaryBoard2026 from "@/public/2026-02-01_Work-Board@0.25x.png";
+import MarchBoard2026 from "@/public/2026-03-01_Work-Board@0.25x.png";
 import MainLogo from "@/public/snap-and-code_main-logo.svg";
 
 export default function Projects() {
@@ -22,6 +23,26 @@ export default function Projects() {
     <>
       <main className={styles.main}>
         <h1>Projects</h1>
+        <article className="container">
+          <h2>March 2026</h2>
+          <div className="row">
+            <div className="col-md-2 mt-2 order-md-last">
+              <h3>Programs Used</h3>
+              <ul>
+                <li>Adobe Illustrator</li>
+                <li>Adobe Photoshop</li>
+                <li>Adobe InDesign</li>
+              </ul>
+            </div>
+            <div className="col-md-8 mt-2 order-md-first">
+              <Image
+                src={MarchBoard2026}
+                alt="Projects completed in March 2026"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </article>
         <article className="container">
           <h2>February 2026</h2>
           <div className="row">
