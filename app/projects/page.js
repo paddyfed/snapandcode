01e7ba727145c0de03bfa4bad015e600107c8a67
Paddy <paddyfedoo@hotmail.com>
2026-09-18 23:38:39 +0100

    August 2026

--- app/projects/page.js
+++ app/projects/page.js
@@ -20,6 +20,7 @@ import AprilBoard2026 from "@/public/2026-04-01_Work-Board@0.25x.png";
 import MayBoard2026 from "@/public/2026-05-01_Work-Board@0.25x.png";
 import JuneBoard2026 from "@/public/2026-06-01_Work-Board@0.25x.png";
 import JulyBoard2026 from "@/public/2026-07-01_Work-Board@0.25x.png";
+import AugustBoard2026 from "@/public/2026-08-01_Work-Board@0.25x.png";
 import MainLogo from "@/public/snap-and-code_main-logo.svg";
 import { Video } from "../components/Video";
 
@@ -28,6 +29,28 @@ export default function Projects() {
     <>
       <main className={styles.main}>
         <h1>Projects</h1>
+        <article className="container">
+          <h2>August 2026</h2>
+          <div className="row">
+            <div className="col-md-2 mt-2 order-md-last">
+              <h3>Programs Used</h3>
+              <ul>
+                <li>Adobe After Effects</li>
+                <li>Adobe Illustrator</li>
+                <li>Adobe Premiere Pro</li>
+                <li>Adobe Photoshop</li>
+                <li>Adobe InDesign</li>
+              </ul>
+            </div>
+            <div className="col-md-8 mt-2 order-md-first">
+              <Image
+                src={AugustBoard2026}
+                alt="Projects completed in August 2026"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </article>
         <article className="container">
           <h2>July 2026</h2>
           <div className="row">
